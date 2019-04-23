f3d1e2510403ddc159ecb73667890b562b04d514
MohamadAsfour <mohamad.asfour.0001@gmail.com>
2019-04-23 23:13:38 +0300

    more JS

--- main.js
+++ main.js
@@ -63,3 +63,78 @@ for (let i = 0; i< 10; i++){
     console.log(i);
 
 }
+// while
+let  i = 0;
+while(i < 10){
+    console.log(`while Loop number: ${i}`);
+    i++;
+}
+for(let i = 0; i < todos.length; i++){
+    console.log(todos[i].text);
+
+}
+for(let todo of todos){
+    console.log(todo.text);
+}
+// forEach, map, filter
+todos.forEach(function(todo){
+    console.log(todo.text);
+});
+const todoText = todos.map(function(todo){
+    return todo.text;
+});
+
+const todoCompleted = todos.filter(function(todo){
+    return todo.isCompleted === true;
+ }).map(function(todo){
+    return todo.text;
+})
+console.log(todoCompleted);
+// some new future
+const x = 10;
+const color = x < 10 ? 'red' : 'blue';
+console.log(color);
+
+switch(color){
+    case 'red':
+    console.log('color is red');
+    break;
+    case 'blue':
+    console.log('color is blue');
+    break;
+    default: 
+    console.log('color is not known')
+}
+
+// 
+function addNums(num1, num2){
+console.log(num1 + num2);
+}
+addNums(5, 3);
+// same  above with ES6
+const addNums2  = (num1 = 3, num2 = 7) => num1 + num2;
+console.log(5 + 9);
+// 
+const addNums3 = num1 => num1 + 5;
+console.log(addNums3(5));
+
+//  
+
+function Person(firName, lasName, dob){
+    this.firName = firName;
+    this.lasName = lasName;
+    this.dob = new Date(dob);
+    this.getBirthYear = function(){
+        return this.dob.getFullYear();
+    }
+    this.getFullName = function(){
+        return `${this.firName} ${this,lasName}`;
+    }
+}
+
+// Instantiate object
+
+const person1 = new Person('Mohamad', 'Asfour', 02-12-1980);
+console.log(person1);
+console.log(person1.getBirthYear());
+console.log(person1.getFullName());
